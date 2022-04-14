d060dd72d632e9701f3c97b2a851db74a8a79170
Marco Karpf <marco.karpf@swisscom.com>
2022-04-14 09:30:35 +0200

    implement update logic

--- src/main/java/ch/bbw/pr/crowdfounding/ViewController.java
+++ src/main/java/ch/bbw/pr/crowdfounding/ViewController.java
@@ -13,63 +13,82 @@ import javax.swing.text.View;
 
 /**
  * ViewController
- * 
+ *
  * @author Marco KArpf
  * @version 05.04.2022
  */
 @Controller
 public class ViewController {
-	@Autowired
-	private DonationList list;
+    @Autowired
+    private DonationList list;
 
-	@Autowired
-	public ViewController(DonationList list) {
-		this.list = list;
-	}
+    @Autowired
+    public ViewController(DonationList list) {
+        this.list = list;
+    }
 
-	@GetMapping("/")
-	@RequestMapping(value = {"/", "adddonation"}, method = RequestMethod.GET)
-	public String showCrowdfounding(ModelMap model) {
-		System.out.println("ViewController.showCrowdfounding");
-		model.addAttribute("donation", new Donation());
-		model.addAttribute("donations", list.getDonations());
-		System.out.println(list.getAmount());
-		model.addAttribute("list", list);
-		return "crowdfounding";
-	}
+    @GetMapping("/")
+    public String showCrowdfounding(ModelMap model) {
+        System.out.println("ViewController.showCrowdfounding");
+        model.addAttribute("donation", new Donation());
+        model.addAttribute("donations", list.getDonations());
+        model.addAttribute("list", list);
+        return "crowdfounding";
+    }
 
-	@PostMapping("/adddonation")
-	@RequestMapping(value = {"adddonation"}, method = RequestMethod.POST)
-	public String addSpende( Donation donation, ModelMap model) {
-		System.out.println("ViewController.addSpende");
-		list.getDonations().add(donation);
-		model.addAttribute("donations", list.getDonations());
-		model.addAttribute("list", list);
-		return "crowdfounding";
-	}
-	// create mapping to remove last donation from list
-	@GetMapping("/remove")
-	@RequestMapping(value = {"remove"}, method = RequestMethod.GET)
-	public String removeSpende(ModelMap model) {
-		model.addAttribute("donation", new Donation());
-		System.out.println("ViewController.removeSpende");
-		System.out.println(list.getDonations().size());
-		list.removeLastDonation();
-		model.addAttribute("donations", list.getDonations());
-		model.addAttribute("list", list);
-		return "crowdfounding";
-	}
+    @PostMapping("/adddonation")
+    public String addSpende(Donation donation, ModelMap model) {
+        System.out.println("ViewController.addSpende");
+        list.getDonations().add(donation);
+        model.addAttribute("donations", list.getDonations());
+        model.addAttribute("list", list);
+        return "crowdfounding";
+    }
 
-	@GetMapping("/delete/{id}")
-	@RequestMapping(value="delete/{id}", method = RequestMethod.GET)
-	public String deleteDonation(@PathVariable("id") int id, Model model) {
-		model.addAttribute("donation", new Donation());
-		System.out.println("ViewController.deleteDonation");
-		list.getDonations().remove(id);
-		model.addAttribute("donations", list.getDonations());
-		model.addAttribute("list", list);
-		return "crowdfounding";
-	}
+    // create mapping to remove last donation from list
+    @GetMapping("/remove")
+    public String removeSpende(ModelMap model) {
+        model.addAttribute("donation", new Donation());
+        System.out.println("ViewController.removeSpende");
+        list.removeLastDonation();
+        model.addAttribute("donations", list.getDonations());
+        model.addAttribute("list", list);
+        return "crowdfounding";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteDonation(@PathVariable("id") int id, Model model) {
+        model.addAttribute("donation", new Donation());
+        System.out.println("ViewController.deleteDonationById");
+        list.getDonations().remove(id);
+        model.addAttribute("donations", list.getDonations());
+        model.addAttribute("list", list);
+        return "crowdfounding";
+    }
+
+    //add mapping for updating a donation
+    @PostMapping("/update/{id}")
+    public String updateDonation(@PathVariable("id") int id, Model model, Donation donation) {
+        model.addAttribute("donation", new Donation());
+        System.out.println("ViewController.updateDonation");
+        model.addAttribute("donations", list.getDonations());
+        model.addAttribute("list", list);
+        list.updateDonation(donation, id);
+        return "crowdfounding";
+    }
+
+    @GetMapping("/edit/{id}")
+    @RequestMapping(value = "edit/{id}", method = RequestMethod.GET)
+    public String editDonation(@PathVariable("id") int id, Model model) {
+        for (Donation donation : list.getDonations()) {
+            if (donation.getId() == id) {
+                System.out.println("ViewController.editDonation");
+                model.addAttribute("donation", donation);
+
+            }
+        }
+        return "crowdfounding-edit";
+    }
 }
 
 
--- src/main/java/ch/bbw/pr/crowdfounding/model/DonationList.java
+++ src/main/java/ch/bbw/pr/crowdfounding/model/DonationList.java
@@ -9,38 +9,49 @@ import org.springframework.stereotype.Service;
 
 /**
  * Service with list of Donations
+ *
  * @author Marco KArpf
  * @version 05.04.2022
  */
 @Service
 public class DonationList {
 
-	private List<Donation> donations = new ArrayList<>(Arrays.asList(
-			new Donation(0,"Captain America", "120.0", "captain.america@gmal.com", new Date()),
-			new Donation(1, "Spiderman", "240.90", "spider.man@gmail.com", new Date())
-			));
-	public Double amount;
+    private List<Donation> donations = new ArrayList<>(Arrays.asList(
+            new Donation(0, "Captain America", "120.0", "captain.america@gmal.com", new Date()),
+            new Donation(1, "Spiderman", "240.90", "spider.man@gmail.com", new Date())
+    ));
+    public Double amount;
 
-	public List<Donation> 	getDonations(){
-		if (donations.isEmpty()) {
-			return null;
-		}
-		return donations;
-	}
+    public List<Donation> getDonations() {
+        if (donations.isEmpty()) {
+            return null;
+        }
+        return donations;
+    }
 
-	public void addDonation(Donation value) {
-		donations.add(value);
-	}
+    public void addDonation(Donation value) {
+        donations.add(value);
+    }
 
-	public String getAmount() {
-		this.amount = 0.0;
-		for (Donation donation : donations) {
-			amount += Double.parseDouble(donation.getValue());
-		}
-		return String.valueOf(amount);
-	}
+    public String getAmount() {
+        this.amount = 0.0;
+        for (Donation donation : donations) {
+            amount += Double.parseDouble(donation.getValue());
+        }
+        return String.valueOf(amount);
+    }
 
-	public void removeLastDonation() {
-		donations.remove(donations.size()-1);
-	}
+    public void removeLastDonation() {
+        donations.remove(donations.size() - 1);
+    }
+
+    public void updateDonation(Donation donation, int id) {
+        System.out.println("updating donation");
+        for (Donation d : donations) {
+            if (d.getId() == id) {
+                System.out.println("found id " + d.getId());
+                donations.set(d.getId(), donation);
+            }
+        }
+    }
 }
